40ff2516be09031d740765d795b092e1969469c5
Rudolph Jose Razul <rudolphjose.razul@timesoftsg.com.sg>
2024-05-23 11:49:04 +0800

    Make candidate code the requirement

--- EmployeeService.cs
+++ EmployeeService.cs
@@ -5,23 +5,10 @@ namespace csharp_dependencyinjection
 {
     public class EmployeeService : IEmployeeService
     {
-        private readonly IEmployeeRepository _employeeRepository;
-        private readonly ILeaveRepository _leaveRepository;
-
-        public EmployeeService(IEmployeeRepository employeeRepository, ILeaveRepository leaveRepository)
+        public Task<Employee> GetInfoWithLeaveDataByEmployeeId(int employeeId)
         {
-            _employeeRepository = employeeRepository;
-            _leaveRepository = leaveRepository;
+            //your code here
+            throw new NotImplementedException();
         }
-
-        public async Task<Employee> GetInfoWithLeaveDataByEmployeeId(int employeeId)
-        {
-            var employee = new Employee();
-            employee = await _employeeRepository.GetById(employeeId);
-            employee.Leaves = await _leaveRepository.GetLeaveDataByEmployeeId(employeeId);
-
-            return employee;
-        }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -18,9 +18,18 @@ public static class Program
         //get employee info and leave balance data eg Name, Leave type and balance
         var employeeData =  employeeService.GetInfoWithLeaveDataByEmployeeId(1).Result;
 
-        Console.WriteLine("Employee Name: {0} {1}", employeeData.FirstName, employeeData.LastName);
-        foreach (var leave in employeeData.Leaves)
-            Console.WriteLine("Leave: {0} {1}", leave.Type, leave.Balance);
+        /*
+         Objectives: code the following requirements:
+         1. Display (by writing to console) the Employee service results i.e. Employee Name and its leave data (Type and Balance)
+         e.g.
+         Employee Name: John Doe
+         Leave Type: Annual, Balance: 8.0
+         Leave Type: Medical, Balance: 10.0
+
+         2. Implement the Employee service
+        */
+
+        //your code here
 
         Console.ReadLine();
     }
